893bc20b3fc151bbd5bc02975d889fad170c0ec3
miguelvent <migueldkj@gmail.com>
2023-02-28 10:55:23 -0300

    rotas

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Empresa</title>
+</head>
+<body>
+    <h1>Entreprise</h1>
+
+        <a href="{{ url('/') }}"><button>Home</button></a> 
+        <a href="{{ url('/produtos') }}"><button>Produits</button></a>
+        <a href="{{ url('/empresa') }}"><button>Entreprise</button></a>
+        <a href="{{ url('/contato') }}"><button>Contact</button></a>
+
+    </body>
+
+</html>
